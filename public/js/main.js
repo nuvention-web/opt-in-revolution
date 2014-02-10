222abf7b0b36e56ba985e98e4ba95bf5553fcff6
Allen <allenz456@gmail.com>
2014-02-10 13:25:36 -0600

    added content to empower

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
 
 
-	// Events for About Us Tab views
-	$('.opt-about-pillars-detail').slideUp();
+	// // Events for About Us Tab views
+	// $('.opt-about-pillars-detail').slideUp();
 
-	$('.pillar-employ').on('click', function() {
-		$('.opt-about-pillars-detail').not('#employ').slideUp();
-		$('#employ').slideToggle();
-	});
+	// $('.pillar-employ').on('click', function() {
+	// 	$('.opt-about-pillars-detail').not('#employ').slideUp();
+	// 	$('#employ').slideToggle();
+	// 	$('body').scrollTo('.pillar-detail-wrapper');
+	// });
 
-	$('.pillar-empower').on('click', function() {
-		$('.opt-about-pillars-detail').not('#empower').slideUp();
-		$('#empower').slideToggle();
-	});
+	// $('.pillar-empower').on('click', function() {
+	// 	$('.opt-about-pillars-detail').not('#empower').slideUp();
+	// 	$('#empower').slideToggle();
+	// 	$('body').scrollTo('.pillar-detail-wrapper');
+	// });
 
-	$('.pillar-engage').on('click', function() {
-		$('.opt-about-pillars-detail').not('#engage').slideUp();
-		$('#engage').slideToggle();
-	});
-
+	// $('.pillar-engage').on('click', function() {
+	// 	$('.opt-about-pillars-detail').not('#engage').slideUp();
+	// 	$('#engage').slideToggle();
+	// 	$('body').scrollTo('.pillar-detail-wrapper');
+	// });
 });

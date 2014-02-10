6e208edf90093268e24adf8fa695d0267297565a
Allen <allenz456@gmail.com>
2014-02-09 22:07:43 -0600

    started pillar r detail page, issues with multiple collapse divs showing

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
+  $('.collapse').collapse();
+
+ //  $('#employ').on('show.bs.collapse', function () {
+ //  	console.log('employ');
+ //  		$('.collapse.in').collapse();
+ //  });
+
+ // $('#empower').on('show.bs.collapse', function () {
+ // 	console.log('empower');
+ //  		$('.collapse.in').collapse();
+ //  });
+
+ // $('#engage').on('show.bs.collapse', function () {
+ // 	console.log('engage');
+ //  		$('.collapse.in').collapse();
+ //  });
 
 });
